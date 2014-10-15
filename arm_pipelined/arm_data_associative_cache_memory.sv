fa31eb6f6fbf4df4f7cd58da413e63de6b8e6ae5
MWaug <maxwaug@gmail.com>
2014-10-14 21:44:16 -0700

    Fixed tag bug in data cache. Created and tested a 2 way associative cache

--- arm_pipelined/arm_data_associative_cache_memory.sv
+++ arm_pipelined/arm_data_associative_cache_memory.sv
@@ -1,4 +1,4 @@
-module data_cache_memory #(parameter lines = 65536, parameter tagbits = 14, 
+module data_associative_cache_memory #(parameter lines = 65536, parameter tagbits = 14, 
                            parameter blocksize = 4)
                     (input logic clk, reset, we, 
                      input logic [blocksize*32-1:0] wd, 
@@ -10,18 +10,10 @@ module data_cache_memory #(parameter lines = 65536, parameter tagbits = 14,
   parameter setbits = $clog2(lines);
   parameter blockoffset = $clog2(blocksize);
 
-  logic [tagbits-1:0] tag[lines-1:0]; // n lines x tagbits
-  logic [lines-1:0] v;                // n lines x 1 bit
-  logic [setbits-1:0]  set;             // n lines 16 bit address
-  logic [blocksize*32-1:0] RAM[lines-1:0];        // n lines x 32 bits
-
-  // initial
-  //   begin
-  //     // $readmemh("C:/Users/Max/Desktop/Google Drive/Clay-Wolkin/Testing/ldr_strtest/ldr_str_ri.dat",RAM);
-  //     // $readmemh("D:/Max/Google Drive/Clay-Wolkin/Testing/ldr_strtest/ldr_str_ri.dat",RAM);
-  //     for (int i = 0; i <= lines-1; i++)
-  //       v[i] = 0; 
-  //   end
+  logic [tagbits-1:0] tag[lines-1:0];       // n lines x tagbits
+  logic [lines-1:0] v, dirty;               // n lines x 1 bit
+  logic [setbits-1:0]  set;                 // n lines 16 bit address
+  logic [blocksize*32-1:0] RAM[lines-1:0];  // n lines x 32 bits
 
   // Read the data from the cache immediately
   assign set = a[blocksize+setbits-1:blocksize];
