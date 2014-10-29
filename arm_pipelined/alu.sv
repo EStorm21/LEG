759efbf8921c8ba8d37e29ecbb2d0890f1009b98
Ivan W <iwong@g.hmc.edu>
2014-10-28 22:06:13 -0700

    Modified part of ALU, starting to add ALU decoder in Controller

--- arm_pipelined/alu.sv
+++ arm_pipelined/alu.sv
@@ -30,7 +30,8 @@ module alu(input  logic [31:0] aIn, bIn,
   assign invertB = (ALUControl[3:1] == 3'b001) ||   //SUB, RSB
                    (ALUControl[3:1] == 3'b011) ||   //SBC, RSC
                    (ALUControl[3:0] == 4'b1010) ||  //CMP
-                   (ALUControl[3:0] == 4'b1111);    //MVN
+                   (ALUControl[3:0] == 4'b1111) ||  //MVN
+                   (ALUControl[3:0] == 4'b1110);    //BIC
                  
   always_comb
     casex (ALUControl[3:0]) // -------------------------rename to FUNCT
@@ -48,10 +49,10 @@ module alu(input  logic [31:0] aIn, bIn,
  
   always_comb
     casex (ALUControl[3:0]) // ------------------------ change to 2 bit type of operation
-      4'b0000: Result = a & b; // AND, TST
-      4'b1000: Result = a & b; // AND, TST
-      4'b0001: Result = a ^ b; // EOR, TEQ
-      4'b1001: Result = a ^ b; // EOR, TEQ
+      4'b0000: Result = a & condinvb; // AND, TST
+      4'b1000: Result = a & condinvb; // AND, TST
+      4'b0001: Result = a ^ condinvb; // EOR, TEQ
+      4'b1001: Result = a ^ condinvb; // EOR, TEQ
       4'b0010: Result = sum;   // SUB, RSB
       4'b0011: Result = sum;   // SUB, RSB
       4'b0100: Result = sum;   // ADD, ADC, SBC, RSC
@@ -60,10 +61,10 @@ module alu(input  logic [31:0] aIn, bIn,
       4'b0111: Result = sum;   // ADD, ADC, SBC, RSC
       4'b1010: Result = sum;   // CMP, CMN
       4'b1011: Result = sum;   // CMP, CMN
-      4'b1100: Result = a | b; // ORR
-      4'b1101: Result = b;   // MOV
-      4'b1111: Result = ~b;   // MOV, MVN
-      4'b1110: Result = a & ~b; // BIC
+      4'b1100: Result = a | condinvb; // ORR
+      4'b1101: Result = condinvb;   // MOV
+      4'b1111: Result = condinvb;   // MOV, MVN
+      4'b1110: Result = a & condinvb; // BIC
       //default: Result = 32'bx;
     endcase
 
@@ -143,14 +144,6 @@ module alu(input  logic [31:0] aIn, bIn,
       //default: Result = 32'bx;
     endcase
 
-
-  //assign carry    = ((ALUControl[3:0] == 4'b0100/*add*/ | ALUControl[3:0] == 4'b0010/*sub*/ | ALUControl[3:0] == 4'b0011/*rsc*/ | ALUControl[3:0] == 4'b0101/*adc*/
-  //        | ALUControl[3:0] == 4'b0110/*sbc*/ | ALUControl[3:0] == 4'b0111/*rsc*/ | ALUControl[3:0] == 4'b1010/*cmp*/ | ALUControl[3:0] == 4'b1011/*cmn*/) & sum[32]);
-  //assign overflow = (ALUControl[3:0] == 4'b0100/*add*/ | ALUControl[3:0] == 4'b0010/*sub*/ | ALUControl[3:0] == 4'b0011/*rsc*/ | ALUControl[3:0] == 4'b0101/*adc*/
-  //        | ALUControl[3:0] == 4'b0110/*sbc*/ | ALUControl[3:0] == 4'b0111/*rsc*/ | ALUControl[3:0] == 4'b1010/*cmp*/ | ALUControl[3:0] == 4'b1011/*cmn*/) & 
-  //        ~(a[31] ^ b[31] ^ (ALUControl[3:0] == 4'b0010 /*sub*/ | ALUControl[3:0] == 4'b0011/*rsc*/ | ALUControl[3:0] == 4'b0110/*sbc*/ | ALUControl[3:0] == 4'b0111/*rsc*/
-  //        | ALUControl[3:0] == 4'b1010/*cmp*/)) 
-  //        & (a[31] ^ sum[31]); 
 
   assign Flags = {neg, zero, carry, overflow};
 endmodule
