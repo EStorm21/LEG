5ed69b78082727d7e13afc47adbaa1b12fc73a78
MWaug <maxwaug@gmail.com>
2014-11-22 13:43:30 -0800

    changed cache sizes

--- arm_pipelined/mem_simulation.sv
+++ arm_pipelined/mem_simulation.sv
@@ -46,7 +46,6 @@ module mem_simulation
   // ------------------ Actual Memory ----------------------
   initial
       // $readmemh("C:/Users/Max/Desktop/Google Drive/Clay-Wolkin/Testing/simTest.dat",RAM);
-<<<<<<< HEAD
       // $readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
       // $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/ARM/tests/simTest.dat",RAM); //teqrs
       $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/1000_test/test_1000_7.dat",RAM);
@@ -64,11 +63,6 @@ module mem_simulation
       //$readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
       // $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/ARM/tests/simTest.dat",RAM); //teqrs
       //$readmemh("C:/Users/Max/Desktop/Google Drive/Clay-Wolkin/Testing/1000_test/test_1000_7.dat",RAM);
-=======
-      //$readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
-       $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/ARM/tests/simTest.dat",RAM); //teqrs
-
->>>>>>> 6d364839ac748c5df8846e3c64e3e9381c385138
 
   // Return a block
   // assign rd = {RAM[{a[22:4], 2'b00}], 
--- arm_pipelined/top.sv
+++ arm_pipelined/top.sv
@@ -26,7 +26,7 @@ module top(input  logic        clk, reset,
   logic [3:0] ByteMask = 4'b1111;
   
   // instruction cache with a block size of 4 words and 16 lines
-  instr_cache #(4, 2) 
+  instr_cache #(4, 128) 
     instr_cache(.clk(clk), .reset(reset), .BusReady(BusReadyF),
                 .A(PCF), .HRData(HRData), .RD(InstrF), 
                 .IStall(IStall), .HAddrF(HAddrF), .HRequestF(HRequestF) );
@@ -37,7 +37,7 @@ module top(input  logic        clk, reset,
   //                       .MemtoRegM(MemtoRegM), .Valid(Valid), .a(DataAdrM), .MemBlock(HRData),
   //                       .rd(ReadDataM), .Stall(DStall), .MemRE(MemRE), .HWriteM(HWriteM));
 
-  data_writeback_associative_cache #(4, 2) 
+  data_writeback_associative_cache #(4, 128) 
     data_cache(.clk(clk), .reset(reset), .MemWriteM(MemWriteM), .MemtoRegM(MemtoRegM), 
                .BusReady(BusReadyM), .IStall(IStall), .A(DataAdrM), .WD(WriteDataM),
                .HRData(HRData), .ByteMask(ByteMask), .HWData(HWData), .RD(ReadDataM), .HAddr(HAddrM),
