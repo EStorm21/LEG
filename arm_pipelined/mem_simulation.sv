fd5d6e430467dcbf7f753fce644b42db943b2a34
Ivan W <iwong@g.hmc.edu>
2014-12-02 20:09:32 -0800

    Working on LDM now... -Ivan

--- arm_pipelined/mem_simulation.sv
+++ arm_pipelined/mem_simulation.sv
@@ -47,8 +47,8 @@ module mem_simulation
   initial
       // $readmemh("C:/Users/Max/Desktop/Google Drive/Clay-Wolkin/Testing/simTest.dat",RAM);
       // $readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
-       // $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/ARM/tests/simTest.dat",RAM); //teqrs
-      $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/1000_test/test_1000_7.dat",RAM);
+       $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/ARM/tests/simTest.dat",RAM); //teqrs
+      // $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/1000_test/test_1000_7.dat",RAM);
 
 
   // Return a block
