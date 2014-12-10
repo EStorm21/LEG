c4ac2439ed5ec093425f3b8e4ca4621a7dac9d8e
Ivan W <iwong@g.hmc.edu>
2014-12-09 17:45:09 -0800

    Updated Datapath in Visio, Added comments

--- arm_pipelined/data_selector.sv
+++ arm_pipelined/data_selector.sv
@@ -7,6 +7,7 @@ module data_selector (input  logic       ByteOrWord, //byte or word
 
 /***** Brief Description *******
  *
+ * 32-BIT DATAPATH WRITEBACK STAGE
  * The data_selector is used for LDR, LDRB and LDRH to select the data (including offset)
  * Gets data from memory (ReadDataM) --> (ReadDataRawW) and chooses output accordingly (ReadDataW).
  *
--- arm_pipelined/memory_mask.sv
+++ arm_pipelined/memory_mask.sv
@@ -4,6 +4,13 @@ module memory_mask (input  logic       ByteOrWord, //byte or word
                     input  logic [1:0] ByteOffset, //LSBs of address
               		output logic [3:0] FinalByteMask);
 
+/***** Brief Description *******
+ *
+ * CONTROLLER EXECUTE STAGE
+ * The data_selector is used for STR, STRB and STRH to select the data (including offset)
+ * Gets data from registers and chooses output to write accordingly to memory.
+ *
+ ******************************/
   logic [3:0] ByteMask, HalfwordMask, ByteorWordMask;
   mux4 #(4) byteSelect(4'b0001, 4'b0010, 4'b0100, 4'b1000, ByteOffset, ByteMask); 		// Get byte mask
   mux2 #(4) BoWSelect(4'b1111, ByteMask, ByteOrWord, ByteorWordMask);					// Choose between byte or word mask
