6d513b1ea8c55a53c96a83cfa88f739360abe9d8
MWaug <maxwaug@gmail.com>
2015-02-12 00:03:20 -0800

    mmu working all page sizes without fault handling

--- arm_pipelined/mmu.sv
+++ arm_pipelined/mmu.sv
@@ -13,9 +13,9 @@ module mmu(input  logic clk, reset, MMUExtInt, CPUHRequest, CPUHWrite, HReady,
   mux2 #(32) enableMux(CPUHAddr, HAddrOut, Cont[0], HAddr);
 
   // PHRData flop: Hold onto the previous bus value for current translation
-  flopr #(32) HRDataFlop(clk, reset, HRData, PHRData);
+  flopenr #(32) HRDataFlop(clk, reset, HReady, HRData, PHRData);
 
-  typedef enum logic [3:0] {READY, SECTIONTRANS, SMALLFETCH, LARGEFETCH, 
+  typedef enum logic [3:0] {READY, SECTIONTRANS, FIRSTFETCH, 
                             SMALLTRANS, LARGETRANS, DONE} statetype;
   statetype state, nextstate;
 
@@ -27,10 +27,21 @@ module mmu(input  logic clk, reset, MMUExtInt, CPUHRequest, CPUHWrite, HReady,
   // next state logic
   always_comb
     case (state)
-      READY:        nextstate <= (HReady & CPUHRequest & Enable) ? SECTIONTRANS : READY;
+      READY:        if (~(HReady & CPUHRequest & Enable)) begin 
+                      nextstate <= READY;
+                    end else if(HRData[1:0] == 2'b01) begin
+                      nextstate <= FIRSTFETCH;
+                    end else begin
+                      nextstate <= SECTIONTRANS;
+                    end
       SECTIONTRANS: nextstate <= HReady ? DONE : SECTIONTRANS;
-      SMALLFETCH:   nextstate <= HReady ? SMALLTRANS : SMALLFETCH;
-      LARGEFETCH:   nextstate <= HReady ? LARGETRANS : LARGEFETCH;
+      FIRSTFETCH:   if (~HReady) begin
+                      nextstate <= FIRSTFETCH;
+                    end else if(HRData[1:0] == 2'b01) begin 
+                      nextstate <= LARGETRANS;
+                    end else begin
+                      nextstate <= SMALLTRANS;
+                    end
       SMALLTRANS:   nextstate <= HReady ? DONE : SMALLTRANS;
       LARGETRANS:   nextstate <= HReady ? DONE : LARGETRANS;
       DONE:         nextstate <= READY;
@@ -45,20 +56,23 @@ module mmu(input  logic clk, reset, MMUExtInt, CPUHRequest, CPUHWrite, HReady,
       READY:        HAddrOut <= {TBase,  CPUHAddr[31:20], 2'b00};
       SECTIONTRANS: HAddrOut <= {PHRData[31:20], CPUHAddr[19:0]}; 
       DONE:         HAddrOut <= {TBase,  CPUHAddr[31:20]};
-      // Note: LARGEFETCH and SMALLFETCH give the same output.
-      //       This redundancy will be removed when the case is made structural.
-      LARGEFETCH:   HAddrOut <= {PHRData[31:10], CPUHAddr[19:12], 2'b0};
-      SMALLFETCH:   HAddrOut <= {PHRData[31:10], CPUHAddr[19:12], 2'b0};
+      FIRSTFETCH:   HAddrOut <= {PHRData[31:10], CPUHAddr[19:12], 2'b0};
       SMALLTRANS:   HAddrOut <= {PHRData[31:12], CPUHAddr[11:0]};
       LARGETRANS:   HAddrOut <= {PHRData[31:16], CPUHAddr[15:0]};
       default: HAddrOut <= 32'h9999_9999;
     endcase
 
   // HRequest Logic
-  assign HRequest = (state == SECTIONTRANS) | ( (state == READY) & CPUHRequest );
+  assign HRequest = (state == FIRSTFETCH) |
+                    (state == SMALLTRANS)  |
+                    (state == LARGETRANS)  |
+                    (state == SECTIONTRANS) | 
+                    ( (state == READY) & CPUHRequest );
   // CPUHReady Logic  
   assign CPUHReady = (state == DONE);
   // CPUHWrite Logic
-  assign HWrite = ( (state == SECTIONTRANS) | (state == DONE) ) & CPUHWrite;
+  assign HWrite = ( (state == SECTIONTRANS) |
+                    (state == LARGETRANS)   |
+                    (state == SMALLTRANS) ) & CPUHWrite;
 
 endmodule
