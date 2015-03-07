f29fb6d54d203a5fe344d3cce9dc19fe29d9b0da
EStorm21 <estorm@g.hmc.edu>
2015-03-07 10:54:44 -0800

    Updated mrs test

--- tests/mrs.s
+++ tests/mrs.s
@@ -1,5 +1,6 @@
 .text
 start:
+	subs r0, r0, r0
 	mov r0, #5
 	mov r1, #4
 	add r2, r1, r0
@@ -10,5 +11,11 @@ start:
 	mrsmi R7, CPSR
 	msr CPSR_xc,  r1
 	mrs R8, CPSR
+	msr CPSR_cxsf, r3
+	mrs r9, CPSR
+	msr CPSR_f,  #7
+	mrs r10, CPSR
+	msr CPSR, #1024
+	mrs r11, CPSR
 
 stop:	 b stop
