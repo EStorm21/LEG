cc325be4a5a56d69dfb813c725aaba3853696671
EStorm21 <estorm@g.hmc.edu>
2015-04-23 00:29:24 -0700

    Added lrdb2.s

---
+++ tests/ldrb2.s
@@ -0,0 +1,63 @@
+.text
+start:
+        b reset
+        b undef
+        b swi
+        b pfa
+        b da
+        nop
+        b irq
+        b fiq
+prog:
+	mov r0, #0
+	add r4, r0, #1
+	add r2, r0, #0x100
+	add r3, r3, #160
+	str r0, [r3, #4]	
+	strb r3, [r3, r4, lsl #2]
+	ldrb r1, [r3, r4, lsl #2]
+	mov r3, #0x200
+	str r3, [r3, r4, lsl #2]
+	ldr r5, [r3, r4, lsl #2]
+	str r3, [r3, #0]
+	ldrb r6, [r3, #1]!
+	add r11, r3, r11
+	ldrb r7, [r3], #1
+	add r11, r3, r11
+	add r11, r3, r11	
+	mov r3, #0x200
+	ldr r8, [r3], #4
+	add r11, r3, r11
+	ldr r9, [r3, #-4]!
+	add r11, r3, r11
+	ldrb r10, [r3, r4, lsl #2]!
+	add r11, r3, r11
+	mov r3, #0x200
+	strb r3, [r5], r4, lsl #2
+	add r11, r3, r11
+	mov r3, #0x200
+	ldrb r12, [r5], r4, lsl #2 
+	add r11, r3, r11
+	mov r3, #0x200
+	str r3, [r5], r4, lsl #2
+	add r11, r3, r11
+	mov r3, #0x200
+	ldr r13, [r5], r4, lsl #2
+
+
+stop:    b stop
+
+reset: mov r1, #0
+	mov r2, #0
+	b prog
+
+swi:
+        add r2, r1, r2
+        mov r1, r2
+        movs r15, r14
+undef: b prog
+pfa: b prog
+da: b prog
+irq: b prog
+fiq: b prog
+
