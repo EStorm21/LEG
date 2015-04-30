de2d7044f21a0e5d0cd372a4a70ca1f6a81c6968
Sam Dietrich <sdietrich@hmc.edu>
2015-04-29 20:43:56 -0700

    Added myself to dmem.sv

--- leg_pipelined/dmem.sv
+++ leg_pipelined/dmem.sv
@@ -17,12 +17,13 @@ module dmem
     RAM[i] = 32'b0;
   end
       // $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/simTest.dat",RAM);
-       // $readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
-    $readmemh("F:/Academics/Research Clay Wolkin - ARM v4/LEG/tests/simTest.dat",RAM); //teqrs
-       //$readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
-     // $readmemh("/proj/leg/sim/simTest.dat", RAM);
+      // $readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
+      //$readmemh("F:/Academics/Research Clay Wolkin - ARM v4/LEG/tests/simTest.dat",RAM); //teqrs
+      //$readmemh("C:/Users/estor_000/Downloads/simTest.dat",RAM);
+      $readmemh("C:/Users/Sam/Documents/CW/LEG/LEG/tests/simTest.dat", RAM);
+      // $readmemh("/proj/leg/sim/simTest.dat", RAM);
       // $readmemh("F:/Academics/Research Clay Wolkin - LEG v4/LEG/tests/simTest.dat",RAM); //teqrs
-       // $readmemh("F:/Academics/Research Clay Wolkin - LEG v4/LEG/tests/simTest.dat",RAM); //teqrs
+      // $readmemh("F:/Academics/Research Clay Wolkin - LEG v4/LEG/tests/simTest.dat",RAM); //teqrs
       // $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/mmu/sectionTest.dat",RAM);
       // $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/mmu/python/pagetable.dat",RAM);
       // $readmemh("C:/Users/maxwaug/Google Drive/Clay-Wolkin/Testing/mmu/python/pagetable.dat",RAM);
