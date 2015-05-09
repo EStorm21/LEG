533cfdb207a972cb470fab838314f909883c64e7
hexahedria <hexahedria@gmail.com>
2015-05-08 19:01:38 -0700

    Make checkpointing automatically create necessary directory

--- debug_lockstep/checkpoint.py
+++ debug_lockstep/checkpoint.py
@@ -12,6 +12,11 @@ def makeCheckpointHere(name):
 	print "    Dumping qemu's current state..."
 	qemuDump.fullDump(tmpdir)
 
+	# Setup checkpoint directory if necessary
+	cpdir = "output/checkpoints"
+	if not os.path.isdir(cpdir):
+		os.mkdir(cpdir)
+		
 	# Start ModelSim and make the checkpoint
 	print "Starting ModelSim..."
 	modelsim = subprocess.Popen(['vsim', '-do', 'do checkpoint.tcl {} {}'.format(os.path.abspath(tmpdir), os.path.abspath("output/checkpoints/{}.checkpoint".format(name)))], stdin=open(os.devnull), preexec_fn = os.setpgrp)
