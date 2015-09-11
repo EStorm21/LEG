d7ea8cb3898cfc088393db78681969b374059ec2
Daniel Johnson <hexahedria@gmail.com>
2015-09-10 19:35:35 -0700

    More fixes to debug lockstep

--- debug_lockstep/debug.sh
+++ debug_lockstep/debug.sh
@@ -33,5 +33,5 @@ PYTHONPATH=./:/proj/leg/debugutils/python2.7/install/lib/ \
 	LD_LIBRARY_PATH=/proj/leg/debugutils/python2.7/install/lib/ \
 	/proj/leg/debugutils/gcc-arm-none-eabi-4_9-2015q1/bin/arm-none-eabi-gdb-py -q \
 	-ex "python TEST_FILE=\"$TESTFILE\"" \
-	-ex "python RUN_TESTS=\"$RUNTEST\"" \
+	-ex "python RUN_TESTS=$RUNTEST" \
 	-x debug.py
