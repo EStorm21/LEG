4e8d3c8815f150eac88dc529d7601c79649bd740
Sam Dietrich <sdietrich@hmc.edu>
2016-01-24 15:57:30 -0800

    shifter control logic

--- leg_pipelined/barrel_shifter.sv
+++ leg_pipelined/barrel_shifter.sv
@@ -1,7 +1,10 @@
 module barrel_shifter(input  logic [31:0] a,
                       input  logic [ 4:0] shamt,
+                      input  logic        rrx_in, // C flag if rrx, a[0] otherwise. Special bit for shift of 1.
+                      input  logic        longshift, // 32 or more bit shift for mask
                       input  logic        left, shift, arith,
-                      output logic [31:0] y);
+                      output logic [31:0] y,
+                      output logic        a0, a31, rot0, rot31); // C flag will be selected between these bits
 
   logic [31:0] x, rotOut; // intermediate signals
   logic [4:0] mux5_ctl;
@@ -15,22 +18,27 @@ module barrel_shifter(input  logic [31:0] a,
   // 5:1 ror followed by 8:1 ror from CMOS VLSI Design 4th ed. 
   // First stage is ROR 0-3, +1 for left shift
   // Second stage is ROR 0,4,8,12,16,20,24,28 
-  mux5_onehot sh5(a, {a[0],a[31:1]}, {a[1:0],a[31:2]}, {a[2:0],a[31:3]}, {a[3:0],a[31:4]}, mux5_ctl, 
+  mux5_onehot sh5(a, {rrx_in,a[31:1]}, {a[1:0],a[31:2]}, {a[2:0],a[31:3]}, {a[3:0],a[31:4]}, mux5_ctl, 
                   x);
 
   mux8_onehot sh8(x, {x[3:0],x[31:4]}, {x[7:0],x[31:8]}, {x[11:0],x[31:12]}, {x[15:0],x[31:16]}, {x[19:0],x[31:20]}, {x[23:0],x[31:24]}, {x[27:0],x[31:28]}, mux8_ctl, 
                   rotOut);
 
   // Mask off some bits for shift, fill in with sign or 0
-  shift_mask masker(rotOut, a[31], left, shift, arith, shamt,
+  shift_mask masker(rotOut, a[31], longshift, left, shift, arith, shamt,
                     y);
+
+  assign a0 = a[0];
+  assign sign = a[31];
+  assign rot0 = rotOut[0];
+  assign rot31 = rotOut[31];
 
 endmodule // barrel_shifter
 
 
 // Mask the rotated output to create a shift output based on the type of operation
 module shift_mask(input  logic [31:0] a,
-                  input  logic        sign,
+                  input  logic        sign, longshift,
                   input  logic        left, shift, arith,
                   input  logic  [4:0] shamt,
                   output logic [31:0] y);
@@ -39,7 +47,7 @@ module shift_mask(input  logic [31:0] a,
   logic fill;
 
   // Create mask thermometer
-  bin_to_thermL therm(shamt, thermMask);
+  bin_to_thermL therm(shamt, longshift, thermMask);
 
   // If left shift, we need to reverse the mask bits. Look up "streaming operator"
   assign mask = left ? {<<{thermMask}} : thermMask;
@@ -56,12 +64,13 @@ endmodule // shift_mask
 // Convert binary signal to 32-bit thermometer code, filling in from the msb. 
 // Could be more efficiently implemented with prefix tree as in CMOS VLSI Design. 
 // But that can be saved for later
-module bin_to_thermL(input  logic [4:0] a,
+module bin_to_thermL(input  logic [4:0]  a,
+                     input  logic        longshift,
                      output logic [31:0] y);
   integer i;
   always_comb
     for(i = 0; i < 32; i++) 
-      y[i] = (31-i) < a;
+      y[i] = (31-i) < {longshift, a}; // longshift allows all 32 thermometer bits to be 1
 endmodule // bin_to_thermL
 
 
@@ -71,7 +80,7 @@ module mux5_onehot(input  logic [31:0] a0, a1, a2, a3, a4,
                    input  logic [4:0]  sel,
                    output logic [31:0] y);
   always_comb
-    casex(sel)
+    casez(sel)
       5'b????1 : y = a0;
       5'b???1? : y = a1;
       5'b??1?? : y = a2;
@@ -86,7 +95,7 @@ module mux8_onehot(input  logic [31:0] a0, a1, a2, a3, a4, a5, a6, a7,
                    input  logic [7:0]  sel,
                    output logic [31:0] y);
   always_comb
-    casex(sel)
+    casez(sel)
       8'b???????1 : y = a0;
       8'b??????1? : y = a1;
       8'b?????1?? : y = a2;
--- leg_pipelined/shift_control.sv
+++ leg_pipelined/shift_control.sv
@@ -1,31 +1,76 @@
 module shift_control(input  logic [1:0] shtype,
-                     input  logic [4:0] shamt0,
-                     input  logic       isRtype, isRSRtype, zeroRotate, prevCflag, Csel,
+                     input  logic [4:0] R_shamt0,
+                     input  logic [7:0] RSR_shamt,
+                     input  logic       isRtype, isRSRtype, zeroRotate, prevCflag, multCsel,
+                     input  logic       multCarryIn, a0, a31, rot0, rot31,
                      output logic [4:0] shctl_5, // done
                      output logic [7:0] shctl_8, // done
-                     output logic       shift, left, arith, shifterCarryIn, shifterCarryOut);
+                     output logic       shift, left, arith, longshift, rrx_in, shifterCarryOut);
+
+  logic rrx, longShift, shiftSelect;
+  logic [4:0] R_shamt;
+  logic [5:0] actualShift;
+  assign rrx = (shtype == 2'b11) & ~isRSRtype & (R_shamt0 == 5'b00000);
+  assign R_shamt = rrx ? 5'b00001 : R_shamt0;
+  assign shiftSelect = isRSRtype | isRtype;
+  // Exactly what it says on the tin. Take all the possibilities. Output 0 when we don't use shifter.
+  // Note that LSL and RRX don't shift by 32 even when shift amount is 0
+  assign actualShift = {6{shiftSelect}} & (isRSRtype ? {(|RSR_shamt[7:5]),RSR_shamt[4:0]} 
+                                                     : {((shtype != 2'b00) & (R_shamt == 0)), R_shamt});
+  // A shift of 32+. Tells the shifter to mask everything.
+  assign longshift = actualShift[5];
 
   // Create control signals for barrel shifter based on type of instruction
-  logic zeroShift;
-  assign zeroShift = shamt0 == 5'b00000;
   always_comb 
-    casex({shtype, zeroShift})
-      3'b00_? : {shift, left, arith} = 3'b110; // LSL
-      3'b01_? : {shift, left, arith} = 3'b100; // LSR
-      3'b10_? : {shift, left, arith} = 3'b101; // ASR
-      3'b11_0 : {shift, left, arith} = 3'b001; // ROR
-      3'b11_1 : {shift, left, arith} = 3'b101; // RRX, looks like ASR but we use C instead of sign
-      default : {shift, left, arith} = 3'b110; // shouldn't get here
+    casex({shtype, isRSRtype, rrx})
+      4'b00_?_? : {shift, left, arith} = 3'b110; // LSL
+      4'b01_?_? : {shift, left, arith} = 3'b100; // LSR
+      4'b10_?_? : {shift, left, arith} = 3'b101; // ASR
+      4'b11_1_? : {shift, left, arith} = 3'b001; // ROR
+      4'b11_0_0 : {shift, left, arith} = 3'b001; // ROR
+      4'b11_0_1 : {shift, left, arith} = 3'b101; // RRX, looks like ASR but we use C instead of sign
+      default   : {shift, left, arith} = 3'b110; // shouldn't get here
     endcase
 
-  // Select carry in for RRX based on Csel
-  mux2 #(1) shinmux(prevCflag, shiftCarryIn, Csel, shifterCarryIn);
+  // Select carry in for RRX. If not rrx, always select the lsb of the shifter input
+  mux3 #(1) shinmux(prevCflag, multCarryIn, a0, {~rrx,multCsel}, rrx_in);
 
   // Decode shift amount into rotate mux control signals.
-  logic [4:0] shamt;
-  logic rrx;
-  assign rrx = {shtype,zeroShift} == 3'b11_1;
-  assign shamt = rrx ? 5'b00001 : shamt0; // Probably needs more work here to handle shifts of 32+
-  shift_decoder shdec(shamt, left, shctl_5, shctl_8);
+  shift_decoder shdec(actualShift[4:0], left, shctl_5, shctl_8);
+
+  // Select the proper carry out based on the shift type. This depends on RSR, shift type, and shift amount
+  logic shift0 =  actualShift == 6'b000000;
+  logic shift32 = actualShift == 6'b100000 | rrx;
+  logic shift33 = actualShift  > 6'b100000;
+  always_comb
+    casez({shiftSelect, isRSRtype, shift33, shift32, shift0, shtype})
+    // Shifts by 1-31
+      7'b1_?_000_00 : shifterCarryOut = rot0;  // LSL 
+      7'b1_?_000_01 : shifterCarryOut = rot31; // LSR 
+      7'b1_?_000_10 : shifterCarryOut = rot31; // ASR 
+      7'b1_?_000_11 : shifterCarryOut = rot31; // ROR (rrx forces shift32)
+    // R type shifts by 0 (only LSL)
+      7'b1_0_001_?? : shifterCarryOut = prevCflag;  // LSL 
+    // R type shifts by 32 (including RRX)
+      7'b1_0_?10_00 : shifterCarryOut = prevCflag;  // LSL (should never get here) 
+      7'b1_0_?10_01 : shifterCarryOut = a31; // LSR 
+      7'b1_0_?10_10 : shifterCarryOut = a31; // ASR 
+      7'b1_0_?10_11 : shifterCarryOut = a0;  // RRX
+    // RSR type shifts by 0
+      7'b1_1_001_?? : shifterCarryOut = prevCflag;  // ALL
+    // RSR type shifts by 32
+      7'b1_1_010_00 : shifterCarryOut = a0;  // LSL 
+      7'b1_1_010_01 : shifterCarryOut = a31; // LSR 
+      7'b1_1_010_10 : shifterCarryOut = a31; // ASR 
+      7'b1_1_010_11 : shifterCarryOut = a31; // ROR
+    // RSR type shifts by 33+
+      7'b1_1_100_00 : shifterCarryOut = 0;  // LSL 
+      7'b1_1_100_01 : shifterCarryOut = 0; // LSR 
+      7'b1_1_100_10 : shifterCarryOut = a31; // ASR 
+      7'b1_1_100_11 : shifterCarryOut = rot31; // ROR
+    // Not a shift (including I type)
+      7'b0_?_???_?? : shifterCarryOut = zeroRotate ? prevCflag : a31; // From the immediate
+      default       : shifterCarryOut = prevCflag;
+    endcase
 
 endmodule // shift_control
