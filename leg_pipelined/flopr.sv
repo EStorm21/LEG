3a689f68552f23cd4ca51b96dedec143578fcee1
Sam Dietrich <sdietrich@hmc.edu>
2016-01-28 21:03:43 -0800

    converted flop modules to synchronous reset

--- leg_pipelined/flopr.sv
+++ leg_pipelined/flopr.sv
@@ -3,7 +3,7 @@ module flopr #(parameter WIDTH = 8)
                input  logic [WIDTH-1:0] d, 
                output logic [WIDTH-1:0] q);
 
-  always_ff @(posedge clk, posedge reset)
+  always_ff @(posedge clk)
     if (reset) q <= 0;
     else       q <= d;
 endmodule
--- leg_pipelined/floprc.sv
+++ leg_pipelined/floprc.sv
@@ -3,7 +3,7 @@ module floprc #(parameter WIDTH = 8)
                input  logic [WIDTH-1:0] d, 
                output logic [WIDTH-1:0] q);
 
-  always_ff @(posedge clk, posedge reset)
+  always_ff @(posedge clk)
     if (reset) q <= 0;
     else       
       if (clear) q <= 0;
